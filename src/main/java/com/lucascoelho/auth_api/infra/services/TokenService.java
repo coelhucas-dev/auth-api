7211e964f0fbc185ef436ec62fa5a980160d7629
Lucas Coelho <118553341+coelhucas-dev@users.noreply.github.com>
2024-08-10 17:16:31 -0300

    feat: create validate token

--- src/main/java/com/lucascoelho/auth_api/infra/services/TokenService.java
+++ src/main/java/com/lucascoelho/auth_api/infra/services/TokenService.java
@@ -3,6 +3,7 @@ package com.lucascoelho.auth_api.infra.services;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.lucascoelho.auth_api.domain.users.Users;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -15,20 +16,33 @@ import java.time.ZoneOffset;
 public class TokenService {
 
     @Value("${api.security.secret}")
-    private String secret = "";
+    private String secret;
 
-    private String ISSUER = "auth-api";
-
-    private final Algorithm algorithm = Algorithm.HMAC256(secret);
+    private final String ISSUER = "auth-api";
 
     public String generateToken(Users user) {
         try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
             return JWT.create()
                     .withIssuer(ISSUER)
                     .withSubject(user.getUsername())
                     .withExpiresAt(generateExpirationDate())
                     .sign(algorithm);
         } catch (JWTCreationException e) {
+            return null;
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer(ISSUER)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
             return null;
         }
     }
