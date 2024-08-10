dfa301808805fe8b40d298da78e693f2241b066b
Lucas Coelho <118553341+coelhucas-dev@users.noreply.github.com>
2024-08-10 17:22:12 -0300

    feat: implementsecurity filter internal

--- src/main/java/com/lucascoelho/auth_api/infra/sercurity/SecurityFilter.java
+++ src/main/java/com/lucascoelho/auth_api/infra/sercurity/SecurityFilter.java
@@ -1,25 +1,54 @@
 package com.lucascoelho.auth_api.infra.sercurity;
 
+import com.lucascoelho.auth_api.domain.users.Users;
+import com.lucascoelho.auth_api.infra.services.TokenService;
+import com.lucascoelho.auth_api.repositories.users.UsersRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
+    private final UsersRepository usersRepository;
+
+    private final TokenService tokenService;
+
+    public SecurityFilter(UsersRepository usersRepository, TokenService tokenService) {
+        this.usersRepository = usersRepository;
+        this.tokenService = tokenService;
+    }
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String token = this.recoverToken(request);
+        String login = tokenService.validateToken(token);
 
-
+        if (login != null) {
+            Users user = usersRepository.findByUsername(login).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(user.getRole().toString()));
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(login, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
     }
 
     private String recoverToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
         return authHeader.replace("Bearer ", "");
     }
 }
